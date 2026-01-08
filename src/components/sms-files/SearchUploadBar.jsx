da0d46ff9a419b555c28d028e50c6ad88f1a4733
himalsubedi30 <himal.kritim@gmail.com>
2026-01-08 10:40:35 +0545

    updated frontend codes into main branch

--- src/components/sms-files/SearchUploadBar.jsx
+++ src/components/sms-files/SearchUploadBar.jsx
@@ -3,64 +3,64 @@ import React, { useRef } from "react";
 import { Plus, Filter, Search } from "lucide-react";
 
 export default function SearchUploadBar({
-  search,
-  setSearch,
-  bulkGroupName,
-  setBulkGroupName,
-  handleUpload,
-  uploading
-}) {
-  const fileInputRef = useRef(null);
+                                            search,
+                                            setSearch,
+                                            bulkGroupName,
+                                            setBulkGroupName,
+                                            handleUpload,
+                                            uploading
+                                        }) {
+    const fileInputRef = useRef(null);
 
-  return (
-    <div className="flex flex-col sm:flex-row sm:justify-between gap-4 mb-6">
-  
-      <div className="relative w-full sm:w-72">
-        <input
-          type="text"
-          placeholder="Search group name..."
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          className="w-full border border-gray-300 rounded-xl py-2 pl-10 pr-4 shadow-sm text-sm focus:ring-2 focus:ring-teal-500 focus:border-teal-500 outline-none"
-        />
-        <Search className="w-5 h-5 absolute left-3 top-2.5 text-gray-500" />
-      </div>
+    return (
+        <div className="flex flex-col sm:flex-row sm:justify-between gap-4 mb-6">
 
-     
-      <div className="flex flex-col sm:flex-row gap-3 w-full sm:w-auto">
-       
-        <input
-          type="text"
-          placeholder="Enter group name"
-          value={bulkGroupName}
-          onChange={(e) => setBulkGroupName(e.target.value)}
-          className="w-full sm:w-auto border border-gray-300 rounded-xl py-2 px-3 text-sm shadow-sm focus:ring-2 focus:ring-teal-500 focus:border-teal-500 outline-none"
-          disabled={uploading}
-        />
+            <div className="relative w-full sm:w-72">
+                <input
+                    type="text"
+                    placeholder="Search group name..."
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    className="w-full border border-gray-300 rounded-xl py-2 pl-10 pr-4 shadow-sm text-sm focus:ring-2 focus:ring-teal-500 focus:border-teal-500 outline-none"
+                />
+                <Search className="w-5 h-5 absolute left-3 top-2.5 text-gray-500" />
+            </div>
 
-        <button
-          disabled={uploading}
-          onClick={() => fileInputRef.current?.click()}
-          className="w-full sm:w-auto px-5 py-2 bg-teal-500 text-white font-semibold rounded-lg shadow flex items-center justify-center gap-2 hover:bg-teal-600 disabled:opacity-50 hover:cursor-pointer transition"
-        >
-          <Plus size={16} />
-          {uploading ? "Uploading..." : "ADD FILES"}
-        </button>
-<<<<<<< HEAD
-=======
 
-  
+            <div className="flex flex-col sm:flex-row gap-3 w-full sm:w-auto">
 
-  
->>>>>>> 20d4d711518ad04d72a909ef489c2919d2c60c54
-        <input
-          type="file"
-          ref={fileInputRef}
-          onChange={handleUpload}
-          className="hidden"
-          accept=".csv,.xlsx,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-        />
-      </div>
-    </div>
-  );
+                <input
+                    type="text"
+                    placeholder="Enter group name"
+                    value={bulkGroupName}
+                    onChange={(e) => setBulkGroupName(e.target.value)}
+                    className="w-full sm:w-auto border border-gray-300 rounded-xl py-2 px-3 text-sm shadow-sm focus:ring-2 focus:ring-teal-500 focus:border-teal-500 outline-none"
+                    disabled={uploading}
+                />
+
+                <button
+                    disabled={uploading}
+                    onClick={() => fileInputRef.current?.click()}
+                    className="w-full sm:w-auto px-5 py-2 bg-teal-500 text-white font-semibold rounded-lg shadow flex items-center justify-center gap-2 hover:bg-teal-600 disabled:opacity-50 hover:cursor-pointer transition"
+                >
+                    <Plus size={16} />
+                    {uploading ? "Uploading..." : "ADD FILES"}
+                </button>
+                <<<<<<< HEAD
+                    =======
+
+
+
+
+                >>>>>>> 20d4d711518ad04d72a909ef489c2919d2c60c54
+                    <input
+                        type="file"
+                        ref={fileInputRef}
+                        onChange={handleUpload}
+                        className="hidden"
+                        accept=".csv,.xlsx,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                    />
+            </div>
+        </div>
+);
 }
